90810e53962cabeeb826e3d0c1974c9cb347d29a
tchardin <tdotchardin@gmail.com>
2021-06-25 15:30:25 +0200

    bring back next config

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  // reactStrictMode: true,
+  reactStrictMode: true,
   target: 'serverless',
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/pop/getting-started',
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: '/pop',
-  //       destination: '/pop/getting-started',
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: '/myel-js',
-  //       destination: '/myel-js/getting-started',
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/pop/getting-started',
+        permanent: true,
+      },
+      {
+        source: '/pop',
+        destination: '/pop/getting-started',
+        permanent: true,
+      },
+      {
+        source: '/myel-js',
+        destination: '/myel-js/getting-started',
+        permanent: true,
+      },
+    ];
+  },
 };
